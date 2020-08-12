fdae279249880127a0f50fa660f2a2659297a610
CaKellum <cakellum@bsu.edu>
2020-08-12 02:44:40 -0400

    having trouble with the sql

--- main_app/list.py
+++ main_app/list.py
@@ -13,15 +13,64 @@ def index():
     db = get_db()
     movies = db.execute('SELECT title FROM movies ORDER BY title').fetchall()
     v_games = db.execute('SELECT title FROM v_games ORDER BY title').fetchall()
-    music = db.execute('SELECT album FROM music ORDER BY album')
-    return render_template('listing.html', movies = movies, v_games = v_games, music = music)
+    return render_template('listing.html', movies = movies, v_games = v_games)
 
 
-@bp.route('/add_media', methods=('GET', 'POST'))
-@login_required
-def add_media():
+@bp.route('/add_movies', methods=('GET', 'POST'))
+def add_movies():
     if request.method == 'POST':
-
+        name = request.form["title"]
+        error = None
+        if not name:
+            error = "Title is required."
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute('INSERT INTO v_games title VALUES ?'(name,))
+            db.commit()
             return redirect(url_for('index'))
 
-    return render_template('add_media.html')
+    return render_template('add_media.html', media = 'Movies')
+
+
+@bp.route('/add_v_games', methods=('GET', 'POST'))
+def add_v_games():
+    if request.method == 'POST':
+        name = request.form["title"]
+        error = None
+        if not name:
+            error = "Title is required."
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute('INSERT INTO v_games title VALUES ?', (name,))
+            db.commit()
+            return redirect(url_for('index'))
+
+    return render_template('add_media.html', media = 'Video Games')
+
+
+@bp.route('/add_music', methods=('GET','POST'))
+def add_music():
+    if request.method == 'POST':
+        album = request.form["album"]
+        artist = request.form["artist"]
+        error = None
+        if not album:
+            error = "album is required."
+        if not artist:
+            error = "artist is required."
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE music SET album = ?, artist = ?'
+                (album, artist)
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+    return render_template('add_music.html')
